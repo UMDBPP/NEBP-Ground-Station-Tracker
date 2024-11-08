3f5c049cee6423de5be4bc20be20fc988f2d45df
jesnyder5 <91506097+jesnyder5@users.noreply.github.com>
2024-11-08 13:54:06 -0500

    Fix SDR subprocesses and add source callsign to displayed comment

--- NEBP-Ground-Station-Tracker/Balloon_Coordinates.py
+++ NEBP-Ground-Station-Tracker/Balloon_Coordinates.py
@@ -368,15 +368,11 @@ class Balloon_Coordinates_APRS_SDR(Balloon_Coordinates_APRS):
         rtl_fm_path = Path("/usr/bin/rtl_fm")
         direwolf_path = Path(__file__).parent.parent / "external/direwolf/build/src/direwolf"
         direwolf_conf_path = Path(__file__).parent.parent / "external/direwolf.conf"
-        # self.sdrProc = subprocess.Popen("exec " + str(rtl_fm_path) + " -f 144.39M -o 4 - | " + str(direwolf_path) + " -c " + str(direwolf_conf_path) + " -n 1 -r 24000 -b 16 -",
-        #                                 shell=True,
-        #                                 stdin=subprocess.PIPE,
-        #                                 stdout=subprocess.DEVNULL)
-        self.sdrProc = []
-        self.sdrProc[0] = subprocess.Popen(str(rtl_fm_path) + " -f 144.39M -o 4 -",
+        self.sdrProc = [0, 1]
+        self.sdrProc[0] = subprocess.Popen([str(rtl_fm_path), "-f", "144.39M", "-o", "4", "-"],
                                            stdout=subprocess.PIPE)
         time.sleep(0.1)
-        self.sdrProc[1] = subprocess.Popen(str(direwolf_path) + " -c " + str(direwolf_conf_path) + " -n 1 -r 24000 -b 16 -",
+        self.sdrProc[1] = subprocess.Popen([str(direwolf_path), "-c", str(direwolf_conf_path), "-t", "0", "-n", "1", "-r", "24000", "-b", "16", "-"],
                                            stdin=self.sdrProc[0].stdout)
         time.sleep(0.1)
         super().start()
@@ -384,6 +380,8 @@ class Balloon_Coordinates_APRS_SDR(Balloon_Coordinates_APRS):
 
     def stop(self):
         super().stop()
+        print("Terminating subprocesses...")
+        print("\tTerminating Direwolf subprocess...")
         self.sdrProc[1].terminate()
         self.sdrProc[1].wait(10)
         if self.sdrProc[1].poll() is None:
@@ -392,6 +390,7 @@ class Balloon_Coordinates_APRS_SDR(Balloon_Coordinates_APRS):
             self.sdrProc[1].wait()
         time.sleep(0.1)
         
+        print("\tTerminating RTL_FM subprocess...")
         self.sdrProc[0].terminate()
         self.sdrProc[0].wait(10)
         if self.sdrProc[0].poll() is None:
@@ -399,14 +398,10 @@ class Balloon_Coordinates_APRS_SDR(Balloon_Coordinates_APRS):
             self.sdrProc[0].kill()
             self.sdrProc[0].wait()
         time.sleep(0.1)
-
-        # self.sdrProc.terminate()
-        # self.sdrProc.wait(10)
-        # if self.sdrProc.poll() is None:
-        #     print("Subprocess did not terminate in 10 seconds, sending kill signal")
-        #     self.sdrProc.kill()
-        #     self.sdrProc.wait()
-        # time.sleep(0.1)
+        if (self.sdrProc[1].poll() is None) or (self.sdrProc[0].poll() is None):
+            print("Error terminating subprocesses")
+        else:
+            print("Subprocesses terminated")
 
 
     def _update_coor_alt(self):
@@ -425,9 +420,9 @@ class Balloon_Coordinates_APRS_SDR(Balloon_Coordinates_APRS):
                 # Save received comment
                 if frame.info.comment != None:
                     if type(frame.info.comment) == type(b''):
-                        self._record_comment(frame.info.comment.decode('UTF-8'))
+                        self._record_comment(frame.source.callsign.decode('UTF-8') + "-" + str(frame.source.ssid) + " " + frame.info.comment.decode('UTF-8'))
                     else:
-                        self._record_comment(str(frame.info.comment))
+                        self._record_comment(str(frame.source.callsign) + "-" + str(frame.source.ssid) + " " + str(frame.info.comment))
                 else:
                     self._record_comment("")
 
